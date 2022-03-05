1bdd1c0da409cc599e7bbeb3f4043d17bc30c598
Seungjin Yang <seungjin.yang@cern.ch>
2022-03-05 18:04:15 +0900

    add a repo for HTCondor submit description file

--- setup.sh
+++ setup.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env sh
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-git clone https://github.com/tpope/vim-markdown.git ~/.vim_runtime/my_plugins/vim-markdown
-git clone https://github.com/vim-python/python-syntax.git ~/.vim_runtime/my_plugins/python-syntax
-git clone https://github.com/octol/vim-cpp-enhanced-highlight.git ~/.vim_runtime/my_plugins/vim-cpp-enhanced-highlight
+
+REPO_ARRAY=(
+    https://github.com/tpope/vim-markdown.git
+    https://github.com/vim-python/python-syntax.git
+    https://github.com/octol/vim-cpp-enhanced-highlight.git
+    https://github.com/Gullumluvl/vim-Condor.git
+)
+
+for REPO in ${REPO_ARRAY[@]}; do
+    NAME=${REPO##*/}
+    NAME=${NAME/".git"/""}
+    DIR=~/.vim_runtime/my_plugins/${NAME}
+    git clone ${REPO} ${DIR}
+done
 
 cp -v $(dirname `readlink -f ${BASH_SOURCE[0]}`)/my_configs.vim ~/.vim_runtime
